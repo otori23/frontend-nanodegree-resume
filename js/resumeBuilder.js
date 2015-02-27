d5735c86f0a383ea47a00fc3de6f2a1f8127366f
otori23 <azeez.otori@gmail.com>
2015-02-26 20:31:31 -0800

    update as result of 2nd lesson in JS basics

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,5 @@
-$("#main").append("Joe Bow");
+var formattedName = HTMLheaderName.replace("%data%", "Jane Doette");
+var formattedRole = HTMLheaderRole.replace("%data%", "Artistic Director");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
