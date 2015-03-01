b2a67e3a53de57ef9ef315f907c44dfaa66e1889
otori23 <azeez.otori@gmail.com>
2015-02-28 18:39:12 -0800

    project after lesson 2 complete

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,99 @@
-var formattedName = HTMLheaderName.replace("%data%", "Jane Doette");
-var formattedRole = HTMLheaderRole.replace("%data%", "Artistic Director");
+//var formattedName = HTMLheaderName.replace("%data%", "Jane Doette");
+//var formattedRole = HTMLheaderRole.replace("%data%", "Artistic Director");
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+
+var bio = {
+  "name": "John Doe",
+  "role": "Web Developer",
+  "contacts": {
+    "mobile": "650-55-5555",
+    "email": "john@example.com",
+    "github": "johndoe",
+    "tiwtter": "@johndoe",
+    "location": "San Francisco"
+  },
+  "welcomeMessage": "lorem ipsum dolor sit amet etc etc etc.",
+  "skills": [
+    "awesomeness", "delivering things", "cryogenic sleep", "saving the universe"
+  ],
+  "bioPic": "images/fry.jpg"
+};
+
+var education = {  
+  "schools": [
+      {
+        "name": "Nove Southeastern University",
+        "city": "Fort Lauderdale, FL",
+        "degree": "Masters",
+        "major": ["CS"],
+        "dates": 2013,
+        "url": "http://example.com"
+      },
+      {
+        "name": "Exkerd College",
+        "city": "Saint Petersburg, FL",
+        "degree": "BA",
+        "major": ["CS"],
+        "dates": 2003,
+	"url": "http://example.com"
+      }
+    ],
+    "onlineCourses":
+    {
+      "title": "JavaScript Crash Course",
+      "school": "Udacity",
+      "dates": 2014,
+      "url": "http://www.udaclty.com/course/ud804"
+	}
+};
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Planet Express",
+      "title": "Delivery Boy",
+      "dates": "January 3000 - Future",
+      "description": "Who moved my cheese cheesy feet cauliflower\
+        cheese. Queso taleggio when the cheese comes out everybody\
+        happy airedale ricotta cheese and wine paneer camebert de\
+        normandie. Swiss mozzarella cheese slices feta fromage frais\
+        airedale swiss cheesecake. Hard cheese blue castello halloumi\
+        parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "Panucci's Pizza",
+      "title": "Delivery Boy",
+      "dates": "1998 - December 31, 1999",
+      "description": "Who moved my cheese cheesy feet cauliflower\
+        cheese. Queso taleggio when the cheese comes out everybody\
+        happy airedale ricotta cheese and wine paneer camebert de\
+        normandie. Swiss mozzarella cheese slices feta fromage frais\
+        airedale swiss cheesecake. Hard cheese blue castello halloumi\
+        parmesan say cheese stinking bishop jarlsberg."
+    }
+  ]
+};
+
+var projects = {
+  "projects": [
+    {
+      "title": "Sample Project 1",
+      "dates": "2014",
+      "description": "Who moved my cheese cheesy feet cauliflower\
+        cheese. Queso taleggio when the cheese comes out everybody\
+        happy airedale ricotta cheese and wine paneer camebert de\
+        normandie. Swiss mozzarella cheese slices feta fromage frais\
+        airedale swiss cheesecake. Hard cheese blue castello halloumi\
+        parmesan say cheese stinking bishop jarlsberg.",
+      "images": [
+        "https://lh3.ggpht.com/23-sqOpOGqF06YX3BwIYPIXLX_Ma_clLXySKEHlphqlxr21-\
+	      PPbC80U8SjDi96KTWbNjKfY2Pdq_gyFK9A=s300#w=1757&h=1080",
+        "https://lh3.ggpht.com/23-sqOpOGqF06YX3BwIYPIXLX_Ma_clLXySKEHlphqlxr21-\
+		  PPbC80U8SjDi96KTWbNjKfY2Pdq_gyFK9A=s300#w=1757&h=1080"
+      ]
+    }
+  ]
+};
+
